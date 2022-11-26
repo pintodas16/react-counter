63b471b6a21b6b20a49dcfb11d8d20f05c9337e9
pinto das <pintodas.lu@gmail.com>
2022-11-26 19:43:02 +0600

    counter application are build

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { useState } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const el = document.querySelector("#root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(el);
+
+const Compo = () => {
+  const [count, setCount] = useState(0);
+  const add = () => {
+    setCount(count + 1);
+  };
+  const decrease = () => {
+    setCount(count - 1);
+  };
+  return (
+    <div className="container">
+      <h1 className="title">Counter</h1>;
+      <div className="content">
+        <p className="value">{count}</p>
+        {console.log(count)}
+
+        <div className="all-btn">
+          <button className="btn" onClick={add}>
+            <h5>increase</h5>
+          </button>
+          <button className="btn" onClick={decrease}>
+            <h5>decrease</h5>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+root.render(<Compo />);
